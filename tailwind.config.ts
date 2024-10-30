1dd9c1b94624902a97a41ca44b861f803f2f154a
spring1018 <spring1018nbrandall@gmail.com>
2024-10-30 11:14:51 +0900

    Fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "../../packages/ui/src/**/*.{ts,tsx}",
+    "../../packages/shared-ui/src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
